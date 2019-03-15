39c5b0414cc9f9b3f1af10ed6553e42f5c46390d
cguerrero11 <cguerrero1@my.wctc.edu>
2019-03-15 08:07:14 -0500

    Changed file
    Edited gitignore and main program

--- GitIgnore/Program.cs
+++ GitIgnore/Program.cs
@@ -7,6 +7,7 @@ namespace GitIgnore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("We're learning how to ignore files.");
         }
     }
 }
